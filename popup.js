610dc581cd1998842c1269fa7e0a8ef7989924ab
rnazmo <rnazmo@gmail.com>
2019-09-25 03:00:38 +0900

    Add skeleton

---
+++ popup.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// Ref:
+// https://developer.chrome.com/extensions/tabs#method-query
+// https://developer.mozilla.org/ja/docs/Mozilla/Add-ons/WebExtensions/API/tabs/query
+
+chrome.tabs.query({ active: true, lastFocusedWindow: true }, function(tab) {
+  var result = document.getElementById('result');
+  var activeTab = tab[0];
+
+  var contents = 'title: ' + activeTab.title + '\n';
+  contents += 'url: ' + activeTab.url + '\n';
+
+  // Update textarea.
+  result.value = contents;
+
+  // for debug
+  // console.log(activeTab.title);
+
+  // Select the textarea.
+  result.select();
+});

3a99e7adf09ba2344464d1c6b4eb94310351e055
rnazmo <rnazmo@gmail.com>
2020-03-31 18:22:10 +0900

    Add TODO comment

--- popup.js
+++ popup.js
@@ -9,6 +9,10 @@ chrome.tabs.query({ active: true, lastFocusedWindow: true }, function(tab) {
   var activeTab = tab[0];
 
   // TODO: 特定の URL のとき, title を良い感じに加工する（自分用）
+  // - youtube.com の場合は, Share リンクから見れる短縮URL を使う？
+  // - youtube.com の場合は, （オプション次第で）現在の開始時刻のクエリも付与したURLにしたい
+
+  // TODO: format を自分で指定できるようにしても良いかも. 指定した形式の変数を使って記述してもらう. markdown なら, "[$title]($url)" みたいな. 使える変数はどうする？$title, $url, etc...
   let contents = generateMarkdownLink(activeTab.title, activeTab.url);
 
   // Update textarea.
